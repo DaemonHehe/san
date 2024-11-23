61139e37441470e6be34ecfdc234bf3cd0f77703
unknown <mgsan2163@example.com>
2024-11-23 22:19:57 +0700

    pp

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,7 +14,34 @@ const createProduct = asyncHandler(async (req, res) => {
   }
 });
 
+const updateProduct = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const updateProduct = await Product.findOneAndUpdate(
+      { _id: id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.json(updateProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
+const deleteProduct = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deleteProduct = await Product.findOneAndDelete({ _id: id });
+    res.json(deleteProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 const getSingleProduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
@@ -27,12 +54,18 @@ const getSingleProduct = asyncHandler(async (req, res) => {
 });
 
 const getAllProduct = asyncHandler(async (req, res) => {
-  try{
+  try {
     const getAllProducts = await Product.find();
     res.json(getAllProducts);
-  } catch (error){
+  } catch (error) {
     throw new Error(error);
   }
-})
+});
 
-module.exports = { createProduct, getSingleProduct, getAllProduct };
+module.exports = {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getSingleProduct,
+  getAllProduct,
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,11 +3,17 @@ const {
   createProduct,
   getSingleProduct,
   getAllProduct,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
+const { isAdmin, authMiddleware } = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
-router.post("/", createProduct);
+router.post("/", authMiddleware, isAdmin, createProduct);
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
 router.get("/:id", getSingleProduct);
-router.get("/", getAllProduct)
+router.get("/", getAllProduct);
 
 module.exports = router;
