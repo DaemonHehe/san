4cba47e0e42f9774ca823a7b8df1e1f5ca547173
unknown <mgsan2163@example.com>
2024-12-28 19:28:13 +0700

    728

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
 const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
@@ -12,13 +13,14 @@ const morgan = require("morgan");
 
 dbConnect();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ routes/blogRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { createBlog, updateBlog } = require("../controllers/blogController");
+const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createBlog);
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
